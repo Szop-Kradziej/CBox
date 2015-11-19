9227314ff075d9de147a67507351387e8137b175
Szop-kradziej <karolina.drabarz@gmail.com>
2015-11-19 22:53:47 +0100

    Get input argmunet.

--- sequential_program/main.c
+++ sequential_program/main.c
@@ -1,7 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef int bool;
+#define true  1
+#define false 0
+
+int getInputArgument(int, char**);
+bool isInputArgumentSet(int argc);
+
 int main(int argc, char* argv[]) {
+	
+	int numberOfPolyhedronPoints;
+	
+	numberOfPolyhedronPoints = getInputArgument(argc, argv);
+	printf("Number of polyhedron points: %d\n", numberOfPolyhedronPoints);
 	
 	return 0;
 }
+
+int getInputArgument(int argc, char** argv) {
+	
+	const int ARGUMENT_POSITION = 1;
+	int inputArgument;
+	
+	if(isInputArgumentSet(argc) == false) {
+		printf("You need to set number of polyhedron points!\n");
+		exit(0);
+	}
+	inputArgument = atoi(argv[ARGUMENT_POSITION]);
+	
+	return inputArgument;
+}
+
+bool isInputArgumentSet(int argc) {
+	
+	if(argc > 1) {
+		return true;
+	}
+	return false;
+}
+
